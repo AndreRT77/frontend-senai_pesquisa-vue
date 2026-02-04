c2c636699c9f10f22dc291b1514e1e900ba95879
ANDRÉ ROBERTO TAVARES <25132696@CFP127ALUNO.LOCAL>
2026-02-04 07:40:38 -0300

    Adicionando base no vue config

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/frontend-senai_pesquisa-vue',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
